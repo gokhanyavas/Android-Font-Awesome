07cddb2f36b72041fdcb0262b99b29cce2714449
Gökhan YAVAŞ <yvsgkhn@gmail.com>
2015-11-22 11:27:40 +0200

    Android Font Awesome Kullanımı
    Android Font Awesome Kullanımı

---
+++ app/src/main/java/com/gokhanyavas/fontawesome/MainActivity.java
@@ -0,0 +1,18 @@
+package com.gokhanyavas.fontawesome;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Typeface iconFont = FontManager.getTypeface(getApplicationContext(), FontManager.FONTAWESOME);
+        FontManager.markAsIconContainer(findViewById(R.id.icons_container), iconFont);
+
+    }
+}
